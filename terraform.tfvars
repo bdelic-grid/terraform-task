da09ef037561c925903e3cae8dd175bada2a1d72
Bosko Delic <bdelic@griddynamics.com>
2025-03-06 10:44:03 +0100

    [add] variable descriptions and types

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 project = "gd-gcp-internship-devops"
 startup_script = "./startup_script.sh"
+instance_group_startup_script = "./instance_group_startup_script.sh"
--- variables.tf
+++ variables.tf
@@ -1,11 +1,26 @@
-variable "project" {}
+variable "project" {
+  description = "Name of the project"
+  type = string
+}
 
 variable "region" {
-    default = "us-central1"
+  default = "us-central1"
+  description = "Region of the project"
+  type = string
 }
 
 variable "zone" {
   default = "us-central1-a"
+  description = "Zone of the project"
+  type = string
 }
 
-variable "startup_script" {}
+variable "startup_script" {
+  description = "Path to startup script for VMs"
+  type = string
+}
+
+variable "instance_group_startup_script" {
+  description = "Path to startup script for VMs in the instance group"
+  type = string
+}
