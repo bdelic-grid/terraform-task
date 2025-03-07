f9f88e5906087625fc63c124b043c2995e251362
Bosko Delic <bdelic@griddynamics.com>
2025-03-07 15:32:38 +0100

    [add] providers.tf

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.24.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region = var.region
+  zone = var.zone
+}
+
+provider "null" {
+}
