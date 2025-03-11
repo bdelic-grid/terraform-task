734882170bbb9a4a15fa2ea3fcba7c2511f39eb4
Bosko Delic <bdelic@griddynamics.com>
2025-03-11 12:35:48 +0100

    [refactor] change source_ip_ranges to variable

--- modules/security_policy/variables.tf
+++ modules/security_policy/variables.tf
@@ -2,3 +2,9 @@ variable "policy" {
   description = "Name of the security policy"
   type        = string
 }
+
+variable "source_ip_ranges" {
+  description = "List of allowed IPs"
+  type = list(string)
+  default = ["82.117.193.213", "109.111.235.230"]
+}
