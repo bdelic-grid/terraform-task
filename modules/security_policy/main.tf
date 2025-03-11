2ab931026890a10e991249a567137636a93a6749
Bosko Delic <bdelic@griddynamics.com>
2025-03-11 08:26:07 +0100

    [add] ip filtering

---
+++ modules/security_policy/main.tf
@@ -0,0 +1,29 @@
+resource "google_compute_security_policy" "policy" {
+  name = "bdelic-tf-security-policy"
+
+  rule {
+    description = "Allow traffic from my IPs"
+    action      = "allow"
+    priority    = 1000
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["82.117.193.213", "109.111.235.230"]
+      }
+    }
+  }
+
+  rule {
+    description = "Deny requests from all other IPs"
+    action      = "deny(403)"
+    priority    = 2147483647
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+  }
+
+
+}
